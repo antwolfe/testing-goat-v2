8195a1686aa0857247194351c9fc42acd36dc38f
Anthony <stw@example.com>
2025-04-04 11:52:20 -0500

    all tests passing. localhost site broken

--- superlists/lists/urls.py
+++ superlists/lists/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from lists import views
-from django.urls import path, include
+from django.urls import path
 
 urlpatterns = [
-    path('', views.home_page, name='home'),
-    path('lists/', include('lists.urls'))
+    path('new', views.new_list, name='new_list'),
+    path('<int:list_id>/', views.view_list, name='view_list'),
+    path('<int:list_id>/add_item', views.add_item, name='add_item'),
 ]
