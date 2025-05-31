eb35157a0254c0994695c759ba762e9ff96e88d8
Anthony <stw@example.com>
2025-05-30 22:43:40 -0500

    Use Bootstrap to improve layout

--- superlists/lists/models.py
+++ superlists/lists/models.py
@@ -2,10 +2,13 @@ from django.db import models
 
 
 # Create your models here.
+
+
 class List(models.Model):
     pass
 
 
 class Item(models.Model):
     text = models.TextField(default="")
-    list = models.ForeignKey(List, on_delete=models.SET_DEFAULT, default=None)
+    list = models.ForeignKey(List, on_delete=models.DO_NOTHING, default=None)
+
